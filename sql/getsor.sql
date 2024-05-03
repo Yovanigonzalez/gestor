4d7a11cc1ec253add853ac52b1a29aa3f2e547e4
Yovanigonzalez <yovanigonzar@outlook.com>
2024-05-02 23:36:25 -0600

    bd

--- sql/getsor.sql
+++ sql/getsor.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 02-05-2024 a las 04:26:26
+-- Tiempo de generación: 03-05-2024 a las 04:06:41
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.0.30
 
@@ -43,6 +43,25 @@ INSERT INTO `imagenes` (`id`, `nombre_imagen_login`, `nombre_imagen_recuperar`) 
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `recuperacion`
+--
+
+CREATE TABLE `recuperacion` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(255) NOT NULL,
+  `correo` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Volcado de datos para la tabla `recuperacion`
+--
+
+INSERT INTO `recuperacion` (`id`, `nombre`, `correo`) VALUES
+(5, 'URIEL GONZALEZ RODRIGUEZ', 'asadoslafortaleza2381274286@gmail.com');
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `usuarios`
 --
 
@@ -61,13 +80,12 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id`, `nombre`, `correo`, `contrasena`, `rol`, `estatus`, `fecha_hora`) VALUES
-(10, 'YOVANI GONZALEZ RODRIGUEZ', 'yovanigonzar@gmail.com', '$2y$10$UKhfGPY730jeImF6RPQ7hOgZ2.BSJ7YQo3JVAUhbagEOxfjEA.E76', 'EMPLEADO', 'ACTIVO', '2024-05-01 22:32:52'),
-(11, 'URIEL GONZALEZ RODRIGUEZ', 'april803@yahoo.com', '$2y$10$xiOjDoNS/4OMLCAyup7lFuPyOORMMRWCzOkzbtFNJGlJV66nYURi6', 'EMPLEADO', 'ACTIVO', '2024-05-01 22:33:29'),
-(12, 'YOVANI GONZALEZ RODRIGUEZ', 'A@gmail.com', '$2y$10$CRi5hgBUmI379T8E9n7nFOAZ3Hhg4rsOaVweNe7imCKBWGFGZgTva', 'EMPLEADO', 'ACTIVO', '2024-05-01 22:37:27'),
-(13, 'BLANCA RODRIGUEZ JIMENEZ', 'blanquita@gmail.com', '$2y$10$2bPY2/yZo1FvgEknl64o5OD6PF8CI0JhPsmf8Ae6AtN8YvV/xQgvC', 'EMPLEADO', 'ACTIVO', '2024-05-01 22:52:48'),
-(14, 'JOSE LUIS RODRIGUEZ JIMENEZ', 'admon@mjsolucionesenti.com', '$2y$10$Q5QtgoHS0dlSEcE7bvVxH.v2tTzUr5jDqVH81KZRVm0qlACxbOe5O', 'EMPLEADO', 'ACTIVO', '2024-05-01 23:02:53'),
-(15, 'LAURA TORRES LINARES', 'lau@gmail.com', '$2y$10$I2Gyw8RnRT.1sH0gvP5v/ef6aMa4vleXX2Ymr0mzwgCkT8AgBp3ea', 'ADMIN', 'ACTIVO', '2024-05-01 23:14:32'),
-(16, 'WILLIOM', 'asadoslafortaleza2381274286@gmail.com', '$2y$10$W4KmQ9Hgzau47RgBJVZ5wuANViwcHt2H0vk95rYIx2MOjwAYiWsn6', 'EMPLEADO', 'ACTIVO', '2024-05-01 23:18:08');
+(18, 'LAURA TORRES LINARES', 'lau@gmail.com', '$2y$10$TSM/cEsi.3ww4ZOGD7tDt.di3KbFh7nd9S3SIIGbB1QUDCbN7Ugry', 'ADMIN', 'ACTIVO', '2024-05-02 23:06:53'),
+(19, 'YOVANI GONZALEZ RODRIGUEZ', 'yovanigonzar@gmail.com', '$2y$10$19f8CkGH5NwjYTI4Im6PauLNTtNrQI62HArs62lmF5HKz3rvaVq..', 'EMPLEADO', 'ACTIVO', '2024-05-02 23:07:38'),
+(20, 'LAURA TORRES LINARES', 'giovanigonza88@gmail.com', '$2y$10$1dtkCR8a6emSbubCh4J9oeTs8Q/VTM5c5mRXSz3uz84.AkeYHVyAG', 'EMPLEADO', 'ACTIVO', '2024-05-03 01:33:48'),
+(21, 'JOSE LUIS RODRIGUEZ JIMENEZ', 'april803@yahoo.com', '$2y$10$JW8OKZvsG.liubW08kDU4uzd65FkT/ObYqAsaY94JSabhSdL00v1a', 'EMPLEADO', 'ACTIVO', '2024-05-03 01:34:02'),
+(22, 'LUIS EDUARDO', 'c@gmail.com', '$2y$10$WT7TJ0WHexKhY35ocolwdOuasW7PzLJT92KN3U6Zb7e55MizjpBW2', 'EMPLEADO', 'INACTIVO', '2024-05-03 01:34:13'),
+(23, 'MEZCAL ESPADIN ', 'therichars@gmail.com', '$2y$10$47eRP4hmdXmKvqTufYJtOelOi8qu3PIL.AljvVcNrP8eyKWLZDNPq', 'EMPLEADO', 'INACTIVO', '2024-05-03 01:34:28');
 
 --
 -- Índices para tablas volcadas
@@ -77,6 +95,12 @@ INSERT INTO `usuarios` (`id`, `nombre`, `correo`, `contrasena`, `rol`, `estatus`
 -- Indices de la tabla `imagenes`
 --
 ALTER TABLE `imagenes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `recuperacion`
+--
+ALTER TABLE `recuperacion`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -96,10 +120,16 @@ ALTER TABLE `imagenes`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
+-- AUTO_INCREMENT de la tabla `recuperacion`
+--
+ALTER TABLE `recuperacion`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
